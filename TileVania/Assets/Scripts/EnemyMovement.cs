d839a4bf0fd05d6ea955459bdd7d661aa3dce166
eden337 <eden_337@outlook.com>
2021-05-30 13:46:13 +0300

    TileVania project

---
+++ TileVania/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float speed=1f;
+    Rigidbody2D myRigidBody;
+    Collider2D myCollider2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myCollider2D = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (IsFacingRight())
+        {
+            myRigidBody.velocity = new Vector2(speed, 0f);
+        }
+        else
+        {
+            myRigidBody.velocity = new Vector2(-speed, 0f);
+        }
+    }
+
+    bool IsFacingRight()
+    {
+        return transform.localScale.x > 0;
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)), 1f);
+    }
+}
---
+++ TileVania/Assets/Scripts/Player.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    #region Config
+    [SerializeField] private float speed=1;
+    [SerializeField] private float jumpSpeed = 1;
+    [SerializeField] private float climbSpeed = 5;
+    [SerializeField] private Vector2 deathKick = new Vector2(25f,25f);
+    #endregion
+    #region State
+    bool isAlive = true;
+    bool isClimbing = false;
+    #endregion
+
+    #region Cached Components
+    private Rigidbody2D myRigidbody;
+    private CapsuleCollider2D myBodyCollider;
+    private BoxCollider2D myFeetCollider;
+    private Animator myAnimator;
+    float gravityScaleAtStart;
+    #endregion
+
+    #region Message then methods
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
+        gravityScaleAtStart = myRigidbody.gravityScale;
+        isAlive = true;
+    }
+
+    private void Update()
+    {
+        if (isAlive)
+        {
+            Run();
+            ClimbLadder();
+            Jump();
+            FlipSprite();
+            Die();
+        }
+    }
+
+
+    private void Run()
+    {
+
+        float controlThrow = Input.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * speed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+        
+  
+    }
+
+
+    private void ClimbLadder()
+    {
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ladder")))
+        {
+            myAnimator.SetBool("Climbing", false);
+            myRigidbody.gravityScale = gravityScaleAtStart;
+            return;
+        }
+        myRigidbody.gravityScale = 0;
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 climbVelocity = new Vector2(myRigidbody.velocity.x, verticalInput * climbSpeed);
+        myRigidbody.velocity = climbVelocity;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("Climbing", playerHasVerticalSpeed);
+
+    }
+
+    private void Jump()
+    {
+        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+        if (Input.GetButtonDown("Jump"))
+        { 
+            Vector2 jumpVelocityToAdd = new Vector2(0f,jumpSpeed);
+            myRigidbody.velocity += jumpVelocityToAdd;
+
+        }
+    }
+
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(myRigidbody.velocity.x),1f);
+        }
+    }
+
+    private void Die()
+    {
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy","Traps")))
+        {
+            isAlive = false;
+            myAnimator.SetTrigger("Dying");
+            GetComponent<Rigidbody2D>().velocity = deathKick;
+        }
+    }
+
+
+
+    #endregion
+}
